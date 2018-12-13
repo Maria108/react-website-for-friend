6ce6ccf19b4d2afcaf497604a5371979d930ca6d
Maria108 <maria.abash@gmail.com>
2018-12-13 14:30:39 -0500

    Style pics

--- src/components/home.js
+++ src/components/home.js
@@ -5,13 +5,17 @@ import { Grid } from 'react-bootstrap';
 class Home extends Component {
   render() {
     return (
-      <Grid>
-        <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content
-          or information.
-        </p>
-      </Grid>
+      <div class="home-main">
+        <Grid>
+          <div className="main-info">
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured
+              content or information.
+            </p>
+          </div>
+        </Grid>
+      </div>
     );
   }
 }
