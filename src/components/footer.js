fed885d5ba575b3c3dbf8d2b9de590d7faefdb57
Maria108 <maria.abash@gmail.com>
2018-12-18 14:13:52 -0500

    Footer style

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import './footer.css';
 import { Link } from 'react-router-dom';
+import { Row, Col, Grid, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <ul class="footer-list">
-          <li>
-            <Link to="/instagram">Instagram</Link>
-          </li>
-          <li>
-            <Link to="/About">Facebook</Link>
-          </li>
-          <li>
-            <Link to="/Customers">Telephone</Link>
-          </li>
-        </ul>
+        <Grid className="box-footer">
+          <Row>
+            <Col xs={6} md={6}>
+              <div>
+                <p>Contact</p>
+                <p>370 8th Ave New York</p>
+                <p>917-654-2345</p>
+              </div>
+            </Col>
+            <Col xs={6} md={6}>
+              <div>
+                <p>Visit</p>
+                <Link to="/instagram">Instagram</Link>
+                <p>
+                  <Link to="/About">Facebook</Link>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
       </footer>
     );
   }
