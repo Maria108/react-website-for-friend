1a1906968cded7bbe653839496095dbfa62fd4b4
Maria108 <maria.abash@gmail.com>
2018-12-20 21:15:51 -0500

    Customers page

--- src/components/customers.js
+++ src/components/customers.js
@@ -5,11 +5,69 @@ import { Grid } from 'react-bootstrap';
 class Customers extends Component {
   render() {
     return (
-      <div class="customers-main">
-        <Grid>
-          <div className="custom-info">
-            <h1>Maria A</h1>
-            <p>This is amazing. Feel so much better. Lena is the best therapist in the world!</p>
+      <div>
+        <Grid align="center">
+          <div class="customers-main">
+            <Grid>
+              <div className="custom-info">
+                <p>-This is amazing. Feel so much better. Lena is the best therapist in the world!-</p>
+                <h1 className="testimon">-Maria A-</h1>
+              </div>
+              <div className="custom-info">
+                <p>
+                  This is amazing. Feel so much better. Lena is the best therapist in the world! This is amazing. Feel
+                  so much better.
+                </p>
+                <h1 className="testimon">-Sasha N-</h1>
+              </div>
+              <div className="custom-info">
+                <p>
+                  This is amazing. Feel so much better. Lena is the best therapist in the world! This is amazing. Feel
+                  so much better. This is amazing. Feel so much better.
+                </p>
+                <h1 className="testimon">-Sasha N-</h1>
+              </div>
+              <div className="custom-info">
+                <p>
+                  This is amazing. Feel so much better. Lena is the best therapist in the world! This is amazing. Feel
+                  so much better. This is amazing. Feel so much better. Lena is the best therapist in the world! This is
+                  amazing. Feel so much better.
+                </p>
+                <h1 className="testimon">-Sasha N-</h1>
+              </div>
+            </Grid>
+          </div>
+          <div class="customers-main">
+            <Grid>
+              <div className="custom-info">
+                <p>This is amazing. Feel so much better. Lena is the best therapist in the world!</p>
+                <h1 className="testimon">-Karlee Clos-</h1>
+              </div>
+              <div className="custom-info">
+                <p>This is amazing. Feel so much better. Lena is the best therapist in the world!</p>
+                <h1 className="testimon">-Karlee Clos-</h1>
+              </div>
+              <div className="custom-info">
+                <p>This is amazing. Feel so much better. Lena is the best therapist in the world!</p>
+                <h1 className="testimon">-Karlee Clos-</h1>
+              </div>
+            </Grid>
+          </div>
+          <div class="customers-main">
+            <Grid>
+              <div className="custom-info">
+                <p>This is amazing. Feel so much better. Lena is the best therapist in the world!</p>
+                <h1 className="testimon">-Ashley Gram-</h1>
+              </div>
+            </Grid>
+          </div>
+          <div class="customers-main">
+            <Grid>
+              <div className="custom-info">
+                <p>This is amazing. Feel so much better. Lena is the best therapist in the world!</p>
+                <h1 className="testimon">-Park S-</h1>
+              </div>
+            </Grid>
           </div>
         </Grid>
       </div>
