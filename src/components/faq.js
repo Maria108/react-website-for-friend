2bc15d802dd71ea5ee6c168aa3aaa68488db2936
Maria108 <maria.abash@gmail.com>
2019-01-12 20:41:27 -0500

    FAQ page

---
+++ src/components/faq.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './faq.css';
+import { Grid } from 'react-bootstrap';
+
+class Faq extends Component {
+  render() {
+    return (
+      <div>
+        <Grid>
+          <div className="main-div faq">
+            <h1>Coming soon</h1>
+          </div>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Faq;
