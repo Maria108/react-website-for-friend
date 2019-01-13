31f810e13f69d0b8c9a8f4980a7c39a5aec3fdca
Maria108 <maria.abash@gmail.com>
2019-01-12 20:41:05 -0500

    BLog page

---
+++ src/components/blog.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './blog.css';
+import { Grid } from 'react-bootstrap';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <Grid>
+          <div className="main-div blog">
+            <h1>Coming soon</h1>
+          </div>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Blog;
