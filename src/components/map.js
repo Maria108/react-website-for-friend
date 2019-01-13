4dec545648923d217e108a68186c503ebc2f50f3
Maria108 <maria.abash@gmail.com>
2019-01-12 21:45:57 -0500

    Map

--- src/components/map.js
+++ src/components/map.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <div className="map-icon">{text}</div>;
 
 const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 
