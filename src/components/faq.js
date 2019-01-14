d1c9b05ee6fc1ec1e56857ead9572c57c35c7094
Maria108 <maria.abash@gmail.com>
2019-01-14 12:40:30 -0500

    FAQ page

--- src/components/faq.js
+++ src/components/faq.js
@@ -8,7 +8,7 @@ class Faq extends Component {
       <div>
         <Grid>
           <div className="main-div faq">
-            <h1>Coming soon</h1>
+            <p className="coming">coming soon...</p>
           </div>
         </Grid>
       </div>
